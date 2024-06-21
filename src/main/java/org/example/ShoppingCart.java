5f553335dc6c322c2f4e44294ff1a475a21a8a1d
JuanMolinaRey <Juanmolinarey7@gmail.com>
2024-06-21 12:20:55 +0200

    Refactorizado

--- src/main/java/org/example/ShoppingCart.java
+++ src/main/java/org/example/ShoppingCart.java
@@ -20,21 +20,66 @@ public class ShoppingCart {
     }
 
     private BigDecimal calculatePrice(Product product) {
+        if (product.getNumberOfLegs() != null){
+            return petOfLegs(product);
+        }
+        if (product.getName() != null) {
+            return cards(product);
+        }
+        if (product.getAge()!=null){
+            return  gourmet(product);
+        }
+        if (product.getColor()!=null){
+            return calculateFish(product);
+        }
+
+        return product.getSellPrice();
+    }
+
+    private BigDecimal petOfLegs(Product product){
         if (product.getNumberOfLegs() != null) {
-            return BigDecimal.valueOf(4.2 * product.getNumberOfLegs());
-        } else if (product.getAge() != null) {
-            if (product.isStinky()) {
-                return BigDecimal.valueOf(10.0* product.getAge());
-            } else {
-                return BigDecimal.valueOf(20.0 * product.getAge());
+            if (product.getName().startsWith("The Spider:")){
+
+                BigDecimal price = BigDecimal.valueOf(1.2 * product.getNumberOfLegs());
+
+                if (product.isStinky() && product.getColor()!=null){
+                    switch (product.getColor()) {
+                        case "red" -> price = price.add(BigDecimal.valueOf(2.0)).multiply(BigDecimal.valueOf(0.50));
+                        case "gold" -> price = price.add(BigDecimal.valueOf(3.0)).multiply(BigDecimal.valueOf(0.50));
+                    }
+                }else if (product.isStinky()){
+                    price=BigDecimal.valueOf(1.2 * product.getNumberOfLegs()).multiply(BigDecimal.valueOf(0.50));
+                }else if(product.getColor()!=null) {
+                    switch (product.getColor()) {
+                        case "red" -> price = price.add(BigDecimal.valueOf(2.0));
+                        case "gold" -> price = price.add(BigDecimal.valueOf(3.0));
+                    }
+                }
+                return price;
             }
-        } else if (product.getColor() != null && product.getBasePrice() != null) {
-            return switch (product.getColor()) {
-                case "blue" -> product.getBasePrice().add(BigDecimal.valueOf(0.1));
-                case "gold" -> product.getBasePrice().multiply(BigDecimal.valueOf(100.0));
-                default -> product.getBasePrice();
-            };
-        } else if (product.getName().equals("Magic: The Gathering - Black Lotus")) {
+            return BigDecimal.valueOf(4.2 * product.getNumberOfLegs());
+        }
+        return null;
+    }
+
+    private BigDecimal cards(Product product){
+            if (product.getAge() != null && product.getAge() >= 10) {
+                return switch (product.getColor()) {
+                    case "blue" -> BigDecimal.valueOf(5.0).multiply(BigDecimal.valueOf(0.50));
+                    case "red" -> BigDecimal.valueOf(3.5).multiply(BigDecimal.valueOf(0.50));
+                    default -> BigDecimal.valueOf(2.0);
+                };
+            }
+            if (product.getAge() != null && product.getAge() >= 20) {
+                return switch (product.getColor()) {
+                    case "green" ->
+                            BigDecimal.valueOf(4.40).multiply(BigDecimal.valueOf(0.20)).add(BigDecimal.valueOf(4.40));
+                    case "black" ->
+                            BigDecimal.valueOf(6.80).multiply(BigDecimal.valueOf(0.20)).add(BigDecimal.valueOf(6.80));
+                    default -> BigDecimal.valueOf(2.0);
+                };
+            }
+        if (product.getName().equals("Magic: The Gathering - Black Lotus")) {
             return BigDecimal.valueOf(40000.0);
         } else if (product.getName().startsWith("Magic: The Gathering")) {
             return switch (product.getColor()) {
@@ -42,11 +87,39 @@ public class ShoppingCart {
                 case "red" -> BigDecimal.valueOf(3.5);
                 case "green" -> BigDecimal.valueOf(4.40);
                 case "black" -> BigDecimal.valueOf(6.80);
+                case "brown" -> BigDecimal.valueOf(2.0);
                 default -> BigDecimal.valueOf(2.0);
             };
-        } else {
-            return product.getSellPrice();
         }
+
+        return null;
     }
 
-}
+    private BigDecimal gourmet(Product product){
+
+        if (product.getAge() != null) {
+            if (product.isStinky()) {
+                return BigDecimal.valueOf(10.0 * product.getAge());
+            } else {
+                return BigDecimal.valueOf(20.0 * product.getAge());
+            }
+        }
+        return null;
+    }
+
+    private BigDecimal calculateFish (Product product){
+
+        if (product.getColor() != null && product.getBasePrice() != null) {
+            return switch (product.getColor()) {
+                case "blue" -> product.getBasePrice().add(BigDecimal.valueOf(0.1));
+                case "gold" -> product.getBasePrice().multiply(BigDecimal.valueOf(100.0));
+                default -> product.getBasePrice();
+            };
+        }
+        return null;
+    }
+
+
+    }
+
+
--- src/test/java/org/example/ShoppingCartTest.java
+++ src/test/java/org/example/ShoppingCartTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ShoppingCartTest {
     @Test
-    void calculatePriceForMagicCards_red() {
+    void test_calculate_price_for_magic_cards_red() {
         ShoppingCart shoppingCart = new ShoppingCart();
 
         Product product = new Product(null, null, false, "red", null, "Magic: The Gathering - Mage Red", null);
@@ -19,7 +19,7 @@ class ShoppingCartTest {
 
     }
     @Test
-    void calculatePriceForMagicCards_green() {
+    void test_calculate_price_for_magic_cards_green() {
         ShoppingCart shoppingCart = new ShoppingCart();
 
         Product product = new Product(null, null, false, "green", null, "Magic: The Gathering - Mage Green", null);
@@ -30,7 +30,18 @@ class ShoppingCartTest {
 
     }
     @Test
-    void calculatePriceForMagicCards_default() {
+    void test_calculate_price_for_magic_cards_brown() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, null, false, "brown", null, "Magic: The Gathering - Mage Brown", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 2.0);
+
+    }
+    @Test
+    void test_calculate_price_for_magic_cards_default() {
         ShoppingCart shoppingCart = new ShoppingCart();
 
         Product product = new Product(null, null, false,"null" , null, "Magic: The Gathering - Mage", null);
@@ -42,7 +53,7 @@ class ShoppingCartTest {
     }
 
     @Test
-    void calculatePriceForMagicCards_black_lotus() {
+    void test_calculate_price_for_magic_cards_black_lotus() {
         ShoppingCart shoppingCart = new ShoppingCart();
 
         Product product = new Product(null, null, false, null, null, "Magic: The Gathering - Black Lotus", null);
@@ -54,7 +65,7 @@ class ShoppingCartTest {
     }
 
     @Test
-    void calculatePriceForMagicCards_blue() {
+    void test_calculate_price_for_magic_cards_blue() {
         ShoppingCart shoppingCart = new ShoppingCart();
 
         Product product = new Product(null, null, false, "blue", null, "Magic: The Gathering - Mage Blue", null);
@@ -66,7 +77,7 @@ class ShoppingCartTest {
     }
 
     @Test
-    void calculatePriceForMagicCards_black() {
+    void test_calculate_price_for_magic_cards_black() {
         ShoppingCart shoppingCart = new ShoppingCart();
 
         Product product = new Product(null, null, false, "black", null, "Magic: The Gathering - Mage Black", null);
@@ -76,9 +87,54 @@ class ShoppingCartTest {
         assertEquals(shoppingCart.getTotalPrice(), 6.80);
 
     }
+    @Test
+    void test_if_the_blue_card_is_10_years_old_a_50_percent_reduction_is_applied_to_the_price() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, 10, false, "blue", null, "Magic: The Gathering - Mage Blue", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 2.5);
+
+    }
+    @Test
+    void test_if_the_red_card_is_10_years_old_a_50_percent_reduction_is_applied_to_the_price() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, 10, false, "red", null, "Magic: The Gathering - Mage Red", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 1.75);
+
+    }
+    @Test
+    void test_if_the_green_card_is_20_years_old_a_20_percent_is_increased_to_the_price() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, 20, false, "green", null, "Magic: The Gathering - Mage Green", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 5.28);
+
+    }
 
     @Test
-    void calculatePriceForWine() {
+    void test_if_the_black_card_is_20_years_old_a_20_percent_is_increased_to_the_price() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, 20, false, "black", null, "Magic: The Gathering - Mage Black", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 8.16);
+
+    }
+
+    @Test
+    void test_calculate_price_for_wine() {
         ShoppingCart shoppingCart = new ShoppingCart();
 
         Product product = new Product(null, 5, false, null, null, "el tio juanillo", null);
@@ -90,7 +146,7 @@ class ShoppingCartTest {
     }
 
     @Test
-    void calculatePriceForCheese() {
+    void test_calculate_price_for_cheese() {
         ShoppingCart shoppingCart = new ShoppingCart();
 
         Product product = new Product(null, 5, true, null, null, "French Camembert", null);
@@ -101,7 +157,33 @@ class ShoppingCartTest {
 
     }
     @Test
-    void calculatePriceForNumberOfLegs() {
+    void test_calculate_price_for_blue_fish() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        BigDecimal bigDecimal = new BigDecimal(1);
+
+        Product product = new Product(null, null, false, "blue", bigDecimal, "Paquito", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 1.1);
+
+    }
+    @Test
+    void test_calculate_price_for_gold_fish() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        BigDecimal bigDecimal = new BigDecimal(1);
+
+        Product product = new Product(null, null, false, "gold", bigDecimal, "Brillantin", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 100);
+
+    }
+    @Test
+    void test_if_is_a_pet_the_price_is_4_2_by_legs() {
         ShoppingCart shoppingCart = new ShoppingCart();
 
         Product product = new Product(4, null, false, null, null, "Dog", null);
@@ -113,25 +195,57 @@ class ShoppingCartTest {
     }
 
     @Test
-    void calculatePriceForBlueFish() {
+    void test_if_is_a_spider_the_price_is_1_2_by_legs(){
         ShoppingCart shoppingCart = new ShoppingCart();
-
-        Product product = new Product(null, null, false, "blue", BigDecimal.valueOf(0), "Paquito", null);
+        Product product = new Product(8, null, false, null,null, "The Spider:", null);
 
         shoppingCart.addProduct(product);
 
-        assertEquals(shoppingCart.getTotalPrice(),(0.1));
-
+        assertEquals(shoppingCart.getTotalPrice(), 9.6);
     }
     @Test
-    void calculatePriceForGoldFish() {
+    void test_if_is_a_spider_the_price_is_1_2_by_legs_and_2_for_red_color(){
         ShoppingCart shoppingCart = new ShoppingCart();
-
-        Product product = new Product(null, null, false, "Gold", BigDecimal.valueOf(0), "Brillantin", null);
+        Product product = new Product(8, null, false, "red",null, "The Spider:", null);
 
         shoppingCart.addProduct(product);
 
-        assertEquals(shoppingCart.getTotalPrice(), (0));
+        assertEquals(shoppingCart.getTotalPrice(), 11.6);
+    }
+    @Test
+    void test_if_is_a_spider_the_price_is_1_2_by_legs_and_2_for_gold_color(){
+        ShoppingCart shoppingCart = new ShoppingCart();
+        Product product = new Product(8, null, false, "gold",null, "The Spider:", null);
 
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 12.6);
+    }
+    @Test
+    void test_if_a_red_spider_is_stinky_a_50_percent_reduction_is_applied_to_the_price(){
+        ShoppingCart shoppingCart = new ShoppingCart();
+        Product product = new Product(8, null, true, "red",null, "The Spider:", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 5.8);
+    }
+    @Test
+    void test_if_a_golden_spider_is_stinky_a_50_percent_reduction_is_applied_to_the_price(){
+        ShoppingCart shoppingCart = new ShoppingCart();
+        Product product = new Product(8, null, true, "gold",null, "The Spider:", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 6.3);
+    }
+    @Test
+    void test_if_a_spider_is_stinky_a_50_percent_reduction_is_applied_to_the_price(){
+        ShoppingCart shoppingCart = new ShoppingCart();
+        Product product = new Product(8, null, true, null,null, "The Spider:", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 4.8);
     }
 }
